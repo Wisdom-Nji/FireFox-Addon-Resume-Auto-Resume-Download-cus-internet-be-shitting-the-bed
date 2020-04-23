3f2aea29d4ff8eeb0ea311e21bbcd0b0aa8d8e35
Sherlock Wisdom <wisdomnji@gmail.com>
2020-04-24 00:37:21 +0100

    got it to restart a download

--- main.js
+++ main.js
@@ -5,5 +5,9 @@ downloads.then((downloadItems)=>{
 		console.log(downloadItems[i].id)
 		console.log(downloadItems[i].url)
 		console.log(downloadItems[i].filename)
+
+		console.log("Can Resume:", downloadItems[i].canResume);
+		
+		browser.downloads.download({url: downloadItems[i].url})
 	}
 })
